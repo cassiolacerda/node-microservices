e30abe127a6af55950b0ea1931f822e418edbbb0
Cassio Lacerda <697508+cassiolacerda@users.noreply.github.com>
2020-04-15 19:21:37 -0300

    Conference Project :: Splitting Up a Monolith into Microservices

--- conference/service-registry/server/lib/ServiceRegistry.js
+++ conference/service-registry/server/lib/ServiceRegistry.js
@@ -4,7 +4,7 @@ class ServiceRegistry {
   constructor(log) {
     this.log = log;
     this.services = {};
-    this.timeout = 15;
+    this.timeout = 20;
   }
 
   get(name, version) {
@@ -41,6 +41,9 @@ class ServiceRegistry {
   unregister(name, version, ip, port) {
     const key = name + version + ip + port;
     delete this.services[key];
+    this.log.debug(
+      `Unregistered service ${name}, version ${version} at ${ip}:${port}`
+    );
     return key;
   }
 
